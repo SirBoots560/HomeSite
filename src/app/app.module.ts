c7bdfbb17533f4611feaeea0c4f6014e8778139e
jrl5748 <73135074+jrl5748@users.noreply.github.com>
2021-06-09 10:50:56 -0400

    Init task-item-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskItemComponent } from './task-item/task-item.component';
+import { TaskItemListComponent } from './task-item-list/task-item-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskItemComponent
+    TaskItemComponent,
+    TaskItemListComponent
   ],
   imports: [
     BrowserModule,
