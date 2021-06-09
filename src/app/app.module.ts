0262a3e9bb3a7fe7070e783ab8b17afa7cec6d1c
jrl5748 <73135074+jrl5748@users.noreply.github.com>
2021-06-09 12:39:30 -0400

    Init task-item-form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TaskItemListComponent } from './task-item-list/task-item-list.component';
+import { TaskItemFormComponent } from './task-item-form/task-item-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskItemComponent,
-    TaskItemListComponent
+    TaskItemListComponent,
+    TaskItemFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/task-item-form/task-item-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-item-form',
+  templateUrl: './task-item-form.component.html',
+  styleUrls: ['./task-item-form.component.css']
+})
+export class TaskItemFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
