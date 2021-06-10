da38e3aeb02fdb270a24a04ab4af7daf14aa673a
jrl5748 <73135074+jrl5748@users.noreply.github.com>
2021-06-10 16:32:03 -0400

    Improve backend php stuff

--- backend/www/html/index.php
+++ backend/www/html/index.php
@@ -1,13 +1,32 @@
 <?php
+    //Function for sending response to frontend based on conditionals below
+    function sendResponse($data){
+        header('Access-Control-Allow-Origin: http://localhost', false);
+        header('Content-Type: application/json');
+        echo $data;
+    }
 
 
-function sendResponse($data){
-    header('Access-Control-Allow-Origin: http://localhost', false);
-    header('Content-Type: application/json');
-    echo $data;
-}
+    //Garbage data for dev purposes
+    $TEMP_DATA = array( array(  'id' => 1,
+                                'title' => 'Task',
+                                'category' => 'Cleaning',
+                                'complete' => false),
+                            array(  'id' => 2,
+                                'title' => 'Task-2',
+                                'category' => 'Cleaning',
+                                'complete' => false),
+                            array(  'id' => 3,
+                                'title' => 'Task-3',
+                                'category' => 'Cleaning',
+                                'complete' => false),
+                            array(  'id' => 4,
+                                'title' => 'Task-4',
+                                'category' => 'Cats',
+                                'complete' => false)
+                            );
 
-
+    //Determining the response
     $data;
     $path = ltrim($_SERVER['REQUEST_URI'], '/');    
     $elements = explode('/', $path);                
@@ -15,26 +34,8 @@ function sendResponse($data){
     if($_SERVER['REQUEST_METHOD'] == 'GET'){
         switch(array_shift($elements)){             
 
-            case 'get':                             
-                $data = array( array(  'id' => 1,
-                                            'title' => 'Task',
-                                            'category' => 'Cleaning',
-                                            'complete' => false),
-                                    array(  'id' => 2,
-                                            'title' => 'Task-2',
-                                            'category' => 'Cleaning',
-                                            'complete' => false),
-                                    array(  'id' => 3,
-                                            'title' => 'Task-3',
-                                            'category' => 'Cleaning',
-                                            'complete' => false),
-                                    array(  'id' => 4,
-                                            'title' => 'Task-4',
-                                            'category' => 'Cats',
-                                            'complete' => false)
-                                );
-                                
-                $data = json_encode($data);
+            case 'get':                                                           
+                $data = json_encode($TEMP_DATA);
                 break;
 
             default:                                
@@ -43,6 +44,21 @@ function sendResponse($data){
                 die();
         } 
     } else if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        switch(array_shift($elements)){             
+
+            case 'add':                                                           
+                
+                break;
+
+            case: 'delete':
+
+                break;
+                
+            default:                                
+                http_response_code(404);
+                echo("Error Not Found");
+                die();
+        } 
 
     } else {
         http_response_code(404);
