5d74fa3fc7978fa1bbb940d6f7014ff47f2a1f3e
jrl5748 <73135074+jrl5748@users.noreply.github.com>
2021-06-10 13:29:17 -0400

    Temp backend complete, no DB yet

--- backend/www/html/index.php
+++ backend/www/html/index.php
@@ -1,9 +1,40 @@
-<!DOCTYPE html>  
-<head>  
-    <title>Hello World!</title>
-</head>
+<?php
 
-<body>  
-    <h1>Hello World! Changes are made!</h1>
-    <p><?php echo 'We are running PHP, version: ' . phpversion(); ?></p>
-</body>
+if($_SERVER['REQUEST_METHOD'] == 'GET'){
+
+    $path = ltrim($_SERVER['REQUEST_URI'], '/');    
+    $elements = explode('/', $path);                
+
+    if(empty($elements[0])) {                       
+        echo 'NAILED IT!';
+    } else{ 
+        switch(array_shift($elements)){             
+
+            case 'get':                             
+                $post_data = array( array(  'id' => 1,
+                                            'title' => 'Task',
+                                            'category' => 'Cleaning'),
+                                    array(  'id' => 2,
+                                            'title' => 'Task-2',
+                                            'category' => 'Cleaning'),
+                                    array(  'id' => 3,
+                                            'title' => 'Task-3',
+                                            'category' => 'Cleaning')
+                                );
+        
+                $post_data = json_encode($post_data);
+                break;
+
+            default:                                
+                $include = 'src/php/pages/error.php';
+                $error = "404 - Not Found";
+                $title = 'Error';
+                break;
+        }
+
+        header('Content-Type: application/json');
+        echo $post_data;
+    }
+}
+
+?>
