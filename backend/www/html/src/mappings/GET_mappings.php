1e6fd446f4763116b4eced906cf73e0da963070f
jrl5748 <73135074+jrl5748@users.noreply.github.com>
2021-06-11 12:00:37 -0400

    Get now pulls from DB

--- backend/www/html/src/mappings/GET_mappings.php
+++ backend/www/html/src/mappings/GET_mappings.php
@@ -1,25 +1,28 @@
 <?php
 
-switch($url_components['path']){             
-            
-    case '/get':
-        if($params['category'] == 'All' || $params == null){
-            $data = json_encode($TEMP_DATA); 
-        } else {
-            $filtered_Data = array();
-            foreach($TEMP_DATA as $val){
-                if($params['category'] == $val['category'])
-                    array_push($filtered_Data, $val);
-            }
-            $data = json_encode($filtered_Data);
-        }                                                 
-        
-        break;
-    default:
-        var_dump($url_components['path']);                           
-        http_response_code(404);
-        echo("Error Not Found");
-        die();
-}
+    $SQL = "SELECT * FROM 'tasks'";
+    $result = mysqli_query($conn, "SELECT * FROM tasks");
+    $result = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-sendResponse($data);
+    switch($url_components['path']){             
+        case '/get':
+            if($params['category'] == 'All' || $params == null){
+                $data = json_encode($result); 
+            } else {
+                $filtered_Data = array();
+                foreach($result as $val){
+                    if($params['category'] == $val['category'])
+                        array_push($filtered_Data, $val);
+                }
+                $data = json_encode($filtered_Data);
+            }                                                 
+
+            break;
+        default:
+            var_dump($url_components['path']);                           
+            http_response_code(404);
+            echo("Error Not Found");
+            die();
+    }
+
+    sendResponse($data);
