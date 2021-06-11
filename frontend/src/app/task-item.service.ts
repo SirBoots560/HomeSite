d3d5ea611245355a6162ab988d8c2f1747cfdb44
jrl5748 <73135074+jrl5748@users.noreply.github.com>
2021-06-11 15:50:42 -0400

    Added HTTP error handling

--- frontend/src/app/task-item.service.ts
+++ frontend/src/app/task-item.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -21,19 +22,24 @@ export class TaskItemService implements OnInit{
     const getOptions = {
       params: { category }
     };
-    return this.http.get<TaskItemsResponse>(url, getOptions);
+    return this.http.get<TaskItemsResponse>(url, getOptions).pipe( catchError(this.handleError) );
   }
 
   add(taskItem: any){
     const url = this.url + '/add';
     const param = JSON.stringify(taskItem);
-    return this.http.post(url, param);
+    return this.http.post(url, param).pipe( catchError(this.handleError) );
   }
 
   delete(id: any){
     const param = JSON.stringify(id);
     const url = this.url + `/delete`
-    return this.http.post(url, id);
+    return this.http.post(url, id).pipe( catchError(this.handleError) );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error(error.message);
+    return throwError('A data error occurred, please try again.');
   }
 }
 
