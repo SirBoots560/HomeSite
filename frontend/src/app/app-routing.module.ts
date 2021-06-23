a867a9b69894739be0229a1b965eb88c67677d88
SirBoots560 <lanhamboots@gmail.com>
2021-06-23 09:47:21 -0400

    Implementing temp nav

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { TaskItemFormComponent } from './task-item-form/task-item-form.component';
 import { TaskItemListComponent } from './task-item-list/task-item-list.component';
 import { HomeComponent } from './home/home.component';
+import { LinksComponent } from './links/links.component';
 
 const routes: Routes = [  
 { path: 'add', component: TaskItemFormComponent },
 { path: 'home', component: HomeComponent },
+{ path: 'links', component: LinksComponent },
 { path: ':category', component: TaskItemListComponent },
-{ path: '', pathMatch: 'full', redirectTo: 'all' }];
+{ path: '', pathMatch: 'full', redirectTo: 'home' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
