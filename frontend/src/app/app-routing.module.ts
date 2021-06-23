bfe42bd3a96786bda9a4b167a3a59e657741a639
SirBoots560 <lanhamboots@gmail.com>
2021-06-23 09:00:09 -0400

    Changing some routing and styling

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TaskItemFormComponent } from './task-item-form/task-item-form.component';
 import { TaskItemListComponent } from './task-item-list/task-item-list.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [  
 { path: 'add', component: TaskItemFormComponent },
+{ path: 'home', component: HomeComponent },
 { path: ':category', component: TaskItemListComponent },
 { path: '', pathMatch: 'full', redirectTo: 'all' }];
 
