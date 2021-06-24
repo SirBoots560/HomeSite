3a2857c62a479c92355e2dd49f97abb92af297d3
jrl5748 <jrl5748@psu.edu>
2021-06-24 11:13:47 -0400

    Implementing personilized carousel

--- frontend/src/app/carousel/carousel.component.ts
+++ frontend/src/app/carousel/carousel.component.ts
@@ -2,15 +2,25 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
-  templateUrl: './carousel.component.html'
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent implements OnInit {
 
-  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
-
+  images = [  '../../assets/carousel-images/IMG_5597.jpg',
+              '../../assets/carousel-images/IMG_7649.JPG',
+              '../../assets/carousel-images/IMG_9256.jpg',
+              '../../assets/carousel-images/IMG_3832.jpg',
+              '../../assets/carousel-images/IMG_5430.jpg',
+              '../../assets/carousel-images/IMG_6538.jpg',
+              '../../assets/carousel-images/IMG_7720.jpg',
+              '../../assets/carousel-images/IMG_8648.jpg',
+            ];
+  
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.images);
   }
 
 }
