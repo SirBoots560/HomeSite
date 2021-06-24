e6f0a7c9242ad2d79b58d5e099c6d6731e385aee
jrl5748 <jrl5748@psu.edu>
2021-06-24 11:49:22 -0400

    Fixing image serving

--- frontend/src/app/carousel/carousel.component.ts
+++ frontend/src/app/carousel/carousel.component.ts
@@ -7,14 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
-  images = [  '../../assets/carousel-images/IMG_5597.jpg',
-              '../../assets/carousel-images/IMG_7649.JPG',
-              '../../assets/carousel-images/IMG_9256.jpg',
-              '../../assets/carousel-images/IMG_3832.jpg',
-              '../../assets/carousel-images/IMG_5430.jpg',
-              '../../assets/carousel-images/IMG_6538.jpg',
-              '../../assets/carousel-images/IMG_7720.jpg',
-              '../../assets/carousel-images/IMG_8648.jpg',
+  images = [  'http://localhost:90/carousel-images/IMG_5597.jpg',
+              'http://localhost:90/carousel-images/IMG_7649.JPG',
+              'http://localhost:90/carousel-images/IMG_9256.jpg',
+              'http://localhost:90/carousel-images/IMG_3832.jpg',
+              'http://localhost:90/carousel-images/IMG_5430.jpg',
+              'http://localhost:90/carousel-images/IMG_6538.jpg',
+              'http://localhost:90/carousel-images/IMG_7720.jpg',
+              'http://localhost:90/carousel-images/IMG_8648.jpg',
             ];
   
   constructor() { }
