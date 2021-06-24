80a07bc2dde1b584c8c3306ad0c3949d15612b97
jrl5748 <jrl5748@psu.edu>
2021-06-24 10:31:30 -0400

    Improving the carousel

--- frontend/src/app/carousel/carousel.component.ts
+++ frontend/src/app/carousel/carousel.component.ts
@@ -2,13 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
-  templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  templateUrl: './carousel.component.html'
 })
 export class CarouselComponent implements OnInit {
 
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
-  
+
   constructor() { }
 
   ngOnInit(): void {
