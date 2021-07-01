3c7d8ac4a071e7f1d9249b452fd562afd7e1cf5b
jrl5748 <jrl5748@psu.edu>
2021-07-01 15:54:40 -0400

    Preping for edit link

--- frontend/src/app/providers.ts
+++ frontend/src/app/providers.ts
@@ -1,9 +1,11 @@
 import { InjectionToken } from '@angular/core';
 
+
 export const lookupListToken = new InjectionToken('lookupListToken');
 
 export const lookupLists = {
     categories: ['Cleaning', 'Cats'],
 
-    categories_show: ['All','Cleaning', 'Cats']
+    categories_show: ['All','Cleaning', 'Cats'],
+
 };
